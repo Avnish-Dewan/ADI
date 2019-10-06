56b2577bfed6b3cb3ed1dbe4c19171fa88a21244
Avnish-Dewan <47629050+Avnish-Dewan@users.noreply.github.com>
2019-10-06 13:25:24 +0530

    Create generateBinaryStrings.cpp

---
+++ BackTracking/generateBinaryStrings.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void generateBinaryStrings(string str,int n)
+{
+    if(str.length()==n)
+    {
+            cout<<str<<endl;
+    }
+    else if(str.length()>1)
+    {
+        if(str.at(1)=='1')
+        {
+            generateBinaryStrings(str+"0",n);
+            generateBinaryStrings(str+"1",n);    
+        }
+    }
+    else
+    {
+        generateBinaryStrings(str+"0",n);
+        generateBinaryStrings(str+"1",n);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    string str;
+
+    generateBinaryStrings(str,n); 
+}
